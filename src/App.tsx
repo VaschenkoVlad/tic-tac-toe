514b11c209b4d15f6d1533b447976c0675afb4b1
vladislav <yaneznau000@gmail.com>
2025-09-09 11:24:21 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function App() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
